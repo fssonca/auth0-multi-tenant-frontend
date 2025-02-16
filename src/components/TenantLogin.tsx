887e4985053c6b5a0d2e033cc9e7a24a39157903
Fernando Sousa <fsonca@gmail.com>
2025-02-16 13:27:18 -0300

    fix: fecth orgId

--- src/components/TenantLogin.tsx
+++ src/components/TenantLogin.tsx
@@ -1,14 +1,43 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BASE_API_URL } from "../utils/variables";
 
 function TenantLogin() {
   const { orgSlug } = useParams();
   const { loginWithRedirect } = useAuth0();
 
+  const [orgId, setOrgId] = useState(null);
+
+  useEffect(() => {
+    if (orgSlug) {
+      fetchOrgId();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [orgSlug]);
+
+  const fetchOrgId = async () => {
+    try {
+      const response = await axios.get(
+        `${BASE_API_URL}/organization/${orgSlug}`
+      );
+      console.log(response.data.orgId)
+      setOrgId(response.data.orgId);
+    } catch (error) {
+      console.error("Error fetching organization ID:", error);
+    }
+  };
+
   const handleLogin = async () => {
+    if (!orgId) {
+      console.error("Organization ID is not available yet.");
+      return;
+    }
+
     await loginWithRedirect({
       authorizationParams: {
-        organization: orgSlug,
+        organization: orgId,
       },
     });
   };
@@ -16,7 +45,9 @@ function TenantLogin() {
   return (
     <div>
       <h1>Welcome to '{orgSlug}' Tenant Login</h1>
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogin} disabled={!orgId}>
+        Login
+      </button>
     </div>
   );
 }
--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,2 +1,3 @@
 export const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
 export const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
+export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
